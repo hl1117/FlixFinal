2728d2beccb4b0c4f05d540b765698977c922606
Harika <harika1117@gmail.com>
2018-01-18 16:50:02 -0500

    updated with tabs

---
+++ FlixApp/Views/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  FlixApp
+//
+//  Created by Harika Lingareddy on 1/18/18.
+//  Copyright © 2018 Harika Lingareddy. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
